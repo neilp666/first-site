bab3fd2c2d3a084ca5b4d2427ac5b1f5ee83b598
neil patel <neil@myindianweddingapp.com>
2014-05-24 15:17:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+== README
+
+Landing page demo using bootstrap / html 5
+
+* Ruby 2.0
+
+* Rails 4.0.4
+
+* Bootstrap
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstSite::Application.config.secret_key_base = '52cc994b94dae2af989981a861525bebd3e88ec28472bc8c7c64cbdf418c6355c311ed40b4be5a331efd99f190aa9d0a2cc6b502198cda8bfe38a0d51d53846a'
